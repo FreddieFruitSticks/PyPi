9ab76708bce05d9feea3cbda75f004ac3be56d4a
Freddie <freddieodonnell@afrozaar.com>
2018-06-03 00:35:34 +0200

    semantic analysis for plus or minus

--- src/SemanticAnalyser.cpp
+++ src/SemanticAnalyser.cpp
@@ -1,6 +1,7 @@
 #include<SemanticAnalyser.h>
 #include<AstNode.h>
 #include<Scanner.h>
+#include<Parser.h>
 #include<utility>
 
 std::map<const std::string, std::string[2]> SemanticAnalyser::symbolTable;
@@ -18,8 +19,10 @@ void SemanticAnalyser::processor(AstNode node){
             idNode = *(node.childNodes.front());
             if(node.token.type == FLOAT_DCL){
                 declarationEntry[0] = "float";
+                declarationEntry[1] = "variable";
             }else{
-                declarationEntry[0] = "int";                
+                declarationEntry[0] = "int";     
+                declarationEntry[1] = "variable";     
             }
             symbolTable.insert(std::pair<const std::string, std::string[2]>
                 (idNode.token.value, declarationEntry));
@@ -28,22 +31,41 @@ void SemanticAnalyser::processor(AstNode node){
         }
     }else if(node.token.type == ASSIGN){
         if(node.childNodes.front()->token.type == ID){
-            if(node.childNodes.back()->token.type == INT_NUM){
-
-                AstNode idNode = *node.childNodes.front();
-                AstNode intNumNode = *node.childNodes.back();
-                if(idNode.token.value != ""){
-                    std::string variable = idNode.token.value;
-                    std::map<const std::string, std::string[2]>::iterator it = symbolTable.find(variable);
-                    if(it != SemanticAnalyser::symbolTable.end()){
-                        it->second[1] = intNumNode.token.value;                        
-                    }else{
-                        throw std::runtime_error("Variable not declared");
+            AstNode idNode = *node.childNodes.front();
+            std::string variable = idNode.token.value;
+            std::map<const std::string, std::string[2]>::iterator it = symbolTable.find(variable);
+            
+            if(it != SemanticAnalyser::symbolTable.end()){
+                // std::string declarationEntry[2] = std::get<1>(*it);
+                // std::cout << node.childNodes.back()->token.type << std::endl;
+                if(node.childNodes.back()->token.type == INT_NUM){
+                    if(std::get<1>(*it)[1] != "variable" || std::get<1>(*it)[0] != "int"){
+                        throw std::runtime_error("type mismatch");
                     }
+                }else if(node.childNodes.back()->token.type == FLOAT_NUM){
+                    if(std::get<1>(*it)[1] != "variable" || std::get<1>(*it)[0] != "float"){
+                        throw std::runtime_error("type mismatch");
+                    }
+                }else if(checkOperator(node.childNodes.back()->token.type)){
+                    ;
                 }else{
-                    throw std::runtime_error("Error with parser, no variable name on id token");                
+                    throw std::runtime_error("Cannot assign id to bad type");
                 }
+            }else {
+                throw std::runtime_error("Variable not declared");
             }
+        }
+    }else if(checkOperator(node.token.type)){
+        if(node.childNodes.front()->token.type == INT_NUM){
+            if(node.childNodes.back()->token.type != INT_NUM){
+                throw std::runtime_error("type mismatch for operator");
+            }
+        }else if(node.childNodes.front()->token.type == FLOAT_NUM){
+            if(node.childNodes.back()->token.type != FLOAT_NUM){
+                throw std::runtime_error("type mismatch for operator");
+            }
+        }else{
+            throw std::runtime_error("unsupported types of operator");
         }
     }
 }
--- tests/TestSemantics.cpp
+++ tests/TestSemantics.cpp
@@ -59,8 +59,7 @@ bool testAssign(){
         // std::cout << std::get<1>(*itA)[1] << std::endl;
 
         if(itA != SemanticAnalyser::symbolTable.end()){
-            if(std::get<1>(*itA)[0] == "float" && 
-                std::get<1>(*itA)[1] == "1"){
+            if(std::get<1>(*itA)[0] == "float"){
                 return true;
             }
         }
@@ -70,9 +69,36 @@ bool testAssign(){
     return false;
 }
 
+bool testSumAssign(){
+    try{
+        // Scanner
+        std::vector<TOKEN> tokens = scan(std::string("./tests/test_sources/semantic_analyser/sourceTest3"));
+        TokenStream* tokenStream = new TokenStream(tokens);
+
+        // parser and AST construction
+        AbstractSyntaxTree tree = initialiseAST(tokenStream);
+        AbstractSyntaxTree* treePtr = &tree;
+        
+        // Semantic analysis - type checking and symbol table construction
+        SemanticAnalyser semanticAnalyser;
+        semanticAnalyser.typeChecking(treePtr);
+        std::map<const std::string, std::string[2]>::iterator itA;
+        itA = SemanticAnalyser::symbolTable.find("numA");
+        // std::cout << std::get<1>(*itA)[0] << std::endl;
+        // std::cout << std::get<1>(*itA)[1] << std::endl;
+
+        return true;
+    }catch(std::runtime_error e){
+        std::cerr <<  e.what() << std::endl;
+    }
+    return false;
+}
+
+
 void test_semantics(){
     VERIFY("SourceTest1 - test simple type check", testSimpleTypeCheck());
     VERIFY("SourceTest2 - test assign", testAssign());
+    VERIFY("SourceTest3 - test sum assign", testSumAssign());
 }
 
 // register suite
